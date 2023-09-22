159ef1f84cb35b540cf8413321dacf677c70e0fe
samuelcolares <samuelcolaresdeaquino@gmail.com>
2023-09-22 02:59:46 -0300

    updated skeleton

--- components/ui/gif-card.tsx
+++ components/ui/gif-card.tsx
@@ -1,4 +1,5 @@
 "use state";
+import { Loader2, LoaderIcon } from "lucide-react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
@@ -7,7 +8,9 @@ const GifCard = ({ src }: { src: string }) => {
   useEffect(() => setIsMounted(true), []);
   if (!isMounted)
     return (
-      <div className="lg:w-[16.875rem] lg:h-[16.875rem] w-[8.6663rem] h-[8.6663rem] bg-transparent shadow-gifcard border-[.25rem] border-gifcard rounded-xl overflow-hidden" />
+      <div className="lg:w-[16.875rem] lg:h-[16.875rem] w-[8.6663rem] h-[8.6663rem] bg-transparent shadow-gifcard border-[.25rem] border-gifcard rounded-xl overflow-hidden flex items-center justify-center">
+        <Loader2 className="w-2 h-2 animate-spin" color="#FFF"/>
+      </div>
     );
 
   return (
